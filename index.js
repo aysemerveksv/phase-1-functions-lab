c5e8ed1bd0f60a508f198e519c0ad7aa9e5bde57
Ayşe Merve Kosova <mervekosova1@hotmail.com>
2023-04-24 23:17:18 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+  const hqLocation = 42;
+  return Math.abs(hqLocation - pickupLocation);
+} 
+function distanceFromHqInFeet(pickupLocation) {
+    const numberOfBlocks = distanceFromHqInBlocks(pickupLocation);
+    return numberOfBlocks * 264;
+  } 
+  function distanceTravelledInFeet(startBlock, endBlock) {
+    const distanceInBlocks = Math.abs(endBlock - startBlock);
+    return distanceInBlocks * 264;
+  } 
+  function calculatesFarePrice(startBlock, endBlock) {
+    const distanceInFeet = distanceTravelledInFeet(startBlock, endBlock);
+    let fare;
+  
+    if (distanceInFeet <= 400) {
+      fare = 0;
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+      fare = (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet > 2000 && distanceInFeet < 2500) {
+      fare = 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  
+    return fare;
+  }
+
+  
+  
+  
+  
+  
+  
+  
